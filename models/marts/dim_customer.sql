32af64eec3ca6888be2876973ee5b1ec0948184d
Arun BS <arunbs79@gmail.com>
2025-07-11 06:26:22 +0000

    training last day

--- models/marts/dim_customer.sql
+++ models/marts/dim_customer.sql
@@ -1,31 +1,28 @@
-with customer as (
-select * from {{ ref('stg_customers') }}
+with customers as (
+   select * from {{ ref('stg_customers')}}
 ),
- 
-nation as (
-select * from {{ ref('stg_nations') }}
+orders as (
+   select * from {{ ref('fct_orders')}}
 ),
- 
-region as (
-select * from {{ ref('stg_regions') }}
+customer_orders as (
+   select
+       customer_key as customer_id,
+       min(order_date) as first_order_date,
+       max(order_date) as most_recent_order_date,
+       count(order_key) as number_of_orders,
+       sum(net_item_sales_amount) as lifetime_value
+   from orders
+   group by 1
 ),
 final as (
-select
-        customer.customer_id,
-        customer.name,
-        customer.address,
-        nation.nation_id as nation_id,
-        nation.name as nation,
-        region.region_id as region_id,
-        region.name as region,
-        customer.phone_number,
-        customer.account_balance,
-        customer.market_segment
-        from customer
-        inner join nation
-            on customer.nation_id = nation.nation_id
-        inner join region
-            on nation.region_id = region.region_id
- 
+   select
+       customers.customer_id,
+       customers.name as customer_name,
+       customer_orders.first_order_date,
+       customer_orders.most_recent_order_date,
+       coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+       customer_orders.lifetime_value
+   from customers
+   left join customer_orders using (customer_id)
 )
 select * from final
